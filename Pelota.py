5f595a2f9608035138d5f9a66f28cb1d87b0ef08
camila higuera <mchiguerar@udistrital.edu.co>
2024-03-12 15:42:54 -0500

    version inicial jijiji

---
+++ Pelota.py
@@ -0,0 +1,19 @@
+class Pelota: 
+
+    def __init__(self):
+    
+            self.x
+            self.x
+            self.x
+            self.x
+            self.x       
+            self.x
+
+    def mover(self):
+          pass
+    def rebotar(self):
+          pass
+    def dibujar(self):
+        pass
+    
+
